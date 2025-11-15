692c63dd3676c004b8c48ac550a14aba38e47039
MajOfMyth <connormcgrath06@gmail.com>
2025-11-15 04:20:51 -0600

    fixening pt 1

--- CSENamelessAfterMutant.cs
+++ CSENamelessAfterMutant.cs
@@ -5,9 +5,19 @@ namespace CSENamelessAfterMutant
 {
     public class CSENamelessAfterMutant : Mod
     {
+
+        /*
         public override void Load()
         {
             ChecklistFixer.Checklist.Update();
         }
+        */
+
+        //bad puch leave me alone
+        public override void PostSetupContent()
+        {
+            ChecklistFixer.Checklist.Update();
+        }
+
     }
 }
--- Common/GlobalNPCs/NamelessMutantChanges.cs
+++ Common/GlobalNPCs/NamelessMutantChanges.cs
@@ -1,6 +1,9 @@
 ﻿using Terraria;
 using Terraria.ModLoader;
 using ssm.Core;
+using NoxusBoss.Content.NPCs.Bosses.NamelessDeity;
+using FargowiltasSouls.Content.Bosses.MutantBoss;
+using System;
 
 namespace CSENamelessAfterMutant.Common.GlobalNPCs
 {
@@ -8,7 +11,7 @@ namespace CSENamelessAfterMutant.Common.GlobalNPCs
     {
         public override void SetDefaults(NPC npc)
         {
-            if (npc.type == ModCompatibility.WrathoftheGods.NamelessDeityBoss.Type)
+            if (npc.type == ModContent.NPCType<NamelessDeityBoss>())
             {
 
                 float multiplier = 0;
@@ -23,6 +26,30 @@ namespace CSENamelessAfterMutant.Common.GlobalNPCs
                 npc.lifeMax = (int)(25000000 + (1000000 * multiplier));
                 
             }
+
+            //make mutant a lil weaker for balance
+            if (npc.type == ModContent.NPCType<MutantBoss>())
+            {
+                npc.lifeMax = (int)MathF.Round(npc.lifeMax*0.6f);
+
+            }
+        }
+
+        public override void ModifyIncomingHit(NPC npc, ref NPC.HitModifiers modifiers)
+        {
+
+            if (npc.type == ModContent.NPCType<NamelessDeityBoss>())
+            {
+                //modifiers.FinalDamage *= 10;
+                //this for now
+                modifiers.FinalDamage *= 0.3333f;
+            }
+
+            if (npc.type == ModContent.NPCType<MutantBoss>())
+            {
+                modifiers.FinalDamage *= 0.6f;
+            }
+
         }
     }
 }
