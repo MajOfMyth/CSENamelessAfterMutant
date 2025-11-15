776bbb1575666d6fb2f00a10c9f6607f1d530e22
MajOfMyth <connormcgrath06@gmail.com>
2025-11-15 04:47:56 -0600

    lil cleanup

--- CSENamelessAfterMutant.cs
+++ CSENamelessAfterMutant.cs
@@ -5,19 +5,11 @@ namespace CSENamelessAfterMutant
 {
     public class CSENamelessAfterMutant : Mod
     {
-
-        /*
-        public override void Load()
-        {
-            ChecklistFixer.Checklist.Update();
-        }
-        */
-
         //bad puch leave me alone
+        //public override void Load()
         public override void PostSetupContent()
         {
             ChecklistFixer.Checklist.Update();
         }
-
     }
 }
